5f27f31acff10b6e90ba02f8cd18adb6e0de8b93
KarlisPurins <karlis.purins.1999@gmail.com>
2021-11-23 11:50:58 +0200

    Warehouse Form Layout

---
+++ BidItWarehouse/BidItWarehouse/Warehouse.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace BidItWarehouse
+{
+    partial class Warehouse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtStartPrice = new System.Windows.Forms.TextBox();
+            this.txtImageURL = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblStartingPrice = new System.Windows.Forms.Label();
+            this.lblImageURL = new System.Windows.Forms.Label();
+            this.lblWarehouse = new System.Windows.Forms.Label();
+            this.mnuMainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.warehouseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.btnSubmit.Location = new System.Drawing.Point(271, 353);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(100, 46);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.txtName.Location = new System.Drawing.Point(271, 127);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(177, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.txtDescription.Location = new System.Drawing.Point(271, 181);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescription.Size = new System.Drawing.Size(177, 27);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // txtStartPrice
+            // 
+            this.txtStartPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.txtStartPrice.Location = new System.Drawing.Point(271, 233);
+            this.txtStartPrice.Name = "txtStartPrice";
+            this.txtStartPrice.Size = new System.Drawing.Size(100, 27);
+            this.txtStartPrice.TabIndex = 3;
+            // 
+            // txtImageURL
+            // 
+            this.txtImageURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.txtImageURL.Location = new System.Drawing.Point(271, 285);
+            this.txtImageURL.Name = "txtImageURL";
+            this.txtImageURL.Size = new System.Drawing.Size(482, 27);
+            this.txtImageURL.TabIndex = 4;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblName.Location = new System.Drawing.Point(115, 127);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(64, 25);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblDescription.Location = new System.Drawing.Point(115, 181);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(109, 25);
+            this.lblDescription.TabIndex = 6;
+            this.lblDescription.Text = "Description";
+            // 
+            // lblStartingPrice
+            // 
+            this.lblStartingPrice.AutoSize = true;
+            this.lblStartingPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblStartingPrice.Location = new System.Drawing.Point(115, 233);
+            this.lblStartingPrice.Name = "lblStartingPrice";
+            this.lblStartingPrice.Size = new System.Drawing.Size(123, 25);
+            this.lblStartingPrice.TabIndex = 7;
+            this.lblStartingPrice.Text = "StartingPrice";
+            // 
+            // lblImageURL
+            // 
+            this.lblImageURL.AutoSize = true;
+            this.lblImageURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblImageURL.Location = new System.Drawing.Point(115, 285);
+            this.lblImageURL.Name = "lblImageURL";
+            this.lblImageURL.Size = new System.Drawing.Size(109, 25);
+            this.lblImageURL.TabIndex = 8;
+            this.lblImageURL.Text = "Image URL";
+            // 
+            // lblWarehouse
+            // 
+            this.lblWarehouse.AutoSize = true;
+            this.lblWarehouse.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblWarehouse.Location = new System.Drawing.Point(271, 61);
+            this.lblWarehouse.Name = "lblWarehouse";
+            this.lblWarehouse.Size = new System.Drawing.Size(145, 29);
+            this.lblWarehouse.TabIndex = 9;
+            this.lblWarehouse.Text = "Warehouse";
+            // 
+            // mnuMainMenu
+            // 
+            this.mnuMainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mnuMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMainMenu.Name = "mnuMainMenu";
+            this.mnuMainMenu.Size = new System.Drawing.Size(800, 28);
+            this.mnuMainMenu.TabIndex = 10;
+            this.mnuMainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logoutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(139, 26);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(139, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.warehouseToolStripMenuItem,
+            this.inventoryToolStripMenuItem,
+            this.loginToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // warehouseToolStripMenuItem
+            // 
+            this.warehouseToolStripMenuItem.Name = "warehouseToolStripMenuItem";
+            this.warehouseToolStripMenuItem.Size = new System.Drawing.Size(165, 26);
+            this.warehouseToolStripMenuItem.Text = "Warehouse";
+            // 
+            // inventoryToolStripMenuItem
+            // 
+            this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
+            this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(165, 26);
+            this.inventoryToolStripMenuItem.Text = "Inventory";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(165, 26);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // Warehouse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblWarehouse);
+            this.Controls.Add(this.lblImageURL);
+            this.Controls.Add(this.lblStartingPrice);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtImageURL);
+            this.Controls.Add(this.txtStartPrice);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.mnuMainMenu);
+            this.MainMenuStrip = this.mnuMainMenu;
+            this.Name = "Warehouse";
+            this.Text = "Warehouse";
+            this.Load += new System.EventHandler(this.Warehouse_Load);
+            this.mnuMainMenu.ResumeLayout(false);
+            this.mnuMainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtStartPrice;
+        private System.Windows.Forms.TextBox txtImageURL;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblStartingPrice;
+        private System.Windows.Forms.Label lblImageURL;
+        private System.Windows.Forms.Label lblWarehouse;
+        private System.Windows.Forms.MenuStrip mnuMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem warehouseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
